4554ff0932b46d18bf4f3d1cf452ec8dfcc220c4
sadick254 <sadickjunior@gmail.com>
2020-08-13 15:14:24 +0300

    Fix lint

--- spec/text-editor-registry-spec.js
+++ spec/text-editor-registry-spec.js
@@ -70,7 +70,7 @@ describe('TextEditorRegistry', function() {
 
   describe('.build', function() {
     it('constructs a TextEditor with the right parameters based on its path and text', function() {
-       // await atom.packages.activatePackage('language-javascript');
+      // await atom.packages.activatePackage('language-javascript');
       // await atom.packages.activatePackage('language-c');
 
       atom.config.set('editor.tabLength', 8, { scope: '.source.js' });
